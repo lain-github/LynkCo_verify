e2ced3cd38e57e1eff3ee5e8a345993c9db007d4
grl <18766225813@163.com>
2021-11-22 22:15:54 +0800

    极验验证，领克商城。

--- main.py
+++ main.py
@@ -105,61 +105,8 @@ def getpic():
 
     x = get_offset_distance(cut_image, full_image)
     print(x)
-    # btn = '//div[@class="geetest_slider_button"]'
-    # move(btn, x - 6)
-    #
-    #
-    #
-    # # 读入灰度图
-    # block = cv2.imread(slideimg, 0)
-    # template = cv2.imread(backimg, 0)
-    # # 保存灰度图
-    # blockname = "block.jpg"
-    # templatename = "template.jpg"
-    # cv2.imwrite(blockname, block)
-    # cv2.imwrite(templatename, template)
-    #
-    # contours, hierarchy = cv2.findContours(block, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # image1 = block.copy()
-    # for item in contours:
-    #     rect = cv2.boundingRect(item)
-    #     x = rect[0]
-    #     y = rect[1]
-    #     weight = rect[2]
-    #     height = rect[3]
-    #     cv2.imwrite(blockname, image1[y:y + height, x:x + weight])
-    #     break
-    # cv2.imshow("before", block)
-    # # 滑块再处理
-    # block = cv2.imread(blockname)
-    # block = cv2.cvtColor(block, cv2.COLOR_RGB2GRAY)
-    # # block1 = block.copy()
-    # retval, dst = cv2.threshold(block, 10, 200, cv2.THRESH_BINARY)
-    # # block = cv2.GaussianBlur(block, (3, 3), 1)
-    # # overlapping = cv2.addWeighted(dst, 1, block, 0.5, 0)
-    #
-    # cv2.imwrite(blockname, dst)
-    #
-    # # overlapping = abs(255 - block)
-    # # cv2.imwrite(blockname, overlapping)
-    #
-    # block = cv2.imread(blockname)
-    # template = cv2.imread(templatename)
-    # retval, dst = cv2.threshold(template, 140, 200, cv2.THRESH_BINARY)
-    # result = cv2.matchTemplate(block, dst, cv2.TM_CCOEFF_NORMED)
-    # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-    # print('min_val:', min_val, 'max_val:', max_val)
-    # print('**********', min_loc)
-    # x, y = min_loc
-    # # x, y = np.unravel_index(result.argmax(), result.shape)
-    # print("x is {} y is {}".format(x, y))
-    # print("x方向的偏移", int(y / 1.3), 'x:', x, 'y:', y)
-    # cv2.imshow("template", template)
-    # cv2.imshow("block", block)
-    # cv2.waitKey()
-    # btn = '//div[@class="geetest_slider_button"]'
-    # move(btn, x, int(y))
-    # move(btn, x, y)
+    btn = '//div[@class="geetest_slider_button"]'
+    move(btn, x - 6)
 
 
 def getpersoninfo(channel):
